1bfe3e77e7e3537130d577480b59527963c027c9
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-15 17:53:52 -0500

    stretch agnostic binary search completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -29,21 +29,58 @@ def binary_search(arr, target, start, end):
 
 
 
-
-arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
-
-print(binary_search(arr1, 12, 0, len(arr1)-1))
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-# def agnostic_binary_search(arr, target):
-    # Your code here
+def agnostic_binary_search(arr, target):
+    if arr[0] < arr[1]:
+        left = 0
+        right = len(arr) -1
+        
+        while left <= right:
+
+            middle = (right + left) // 2
+
+            if arr[middle] == target:
+                return middle
+            elif arr[middle] > target:
+                # toss out the left side of the array 
+                right = middle - 1 
+
+            else:
+                left = middle + 1
+                
+        return -1  # not found
+
+    else:
+        left = 0
+        right = len(arr) -1
+        while left <= right:
+            middle = (right + left) // 2
+
+            if arr[middle] == target:
+                return middle
+            elif arr[middle] > target:  # 45 < -17
+                # toss out the left side of the array 
+                left = middle + 1 
+                
+            else:
+                right = middle - 1
+                
+                
+        return -1  # not found
 
 
 
+
+
+ascending = [2, 4, 12, 14, 17, 30, 46, 47, 51, 54, 61]
+descending = [101, 98, 57, 49, 45, 13, -3, -17, -61]
+
+print(agnostic_binary_search(ascending, 47))
 
 
 ## works but does not return the index, returns the found number 
